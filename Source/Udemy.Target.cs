1e5157f0e1528349087d5f14479e5c05e019e58a
AxlMan31 <axel.woermann@gmail.com>
2023-07-25 21:34:37 +0200

    Initial Unreal Setup

---
+++ Source/Udemy.Target.cs
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class UdemyTarget : TargetRules
+{
+	public UdemyTarget( TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Game;
+		DefaultBuildSettings = BuildSettingsVersion.V2;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_1;
+		ExtraModuleNames.Add("Udemy");
+	}
+}
---
+++ Source/Udemy/UdemyGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UdemyGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UDEMY_API AUdemyGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
