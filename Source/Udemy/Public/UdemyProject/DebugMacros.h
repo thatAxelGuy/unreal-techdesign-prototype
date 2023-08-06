5f0e060153e1703dc32a8966e07c5fd179387fc0
Axel Woermann <53866066+AxlMan31@users.noreply.github.com>
2023-08-06 11:03:10 +0200

    Squashed commit of the following:
    commit 0875e161c433dce04f6fa0e1290bb1edf6ad12ab
    Author: AxlMan31 <axel.woermann@gmail.com>
    Date:   Thu Aug 3 22:19:54 2023 +0200
    
        !A Added Avg template function
    
        Avg template function takes two inputs of various types that allow addition and division and adds them and divides them by two to return the average. Fixed issue with DRAW_POINT_SingleFrame #define

--- Source/Udemy/Public/UdemyProject/DebugMacros.h
+++ Source/Udemy/Public/UdemyProject/DebugMacros.h
@@ -5,7 +5,8 @@
 
 #define DRAW_SPHERE(Location, Color) if(GetWorld()) DrawDebugSphere(GetWorld(), Location, 25.f, 12, Color, true);
 #define DRAW_LINE(StartLocation, EndLocation) if(GetWorld()) DrawDebugLine(GetWorld(), StartLocation, EndLocation, FColor::Red, true, -1.f, 0, 1.f);
-#define DRAW_POINT(Location) if(GetWorld()) DrawDebugPoint(World, Location, 15.f, FColor::Green, true, -1.f, 0);
+#define DRAW_POINT(Location) if(GetWorld()) DrawDebugPoint(GetWorld(), Location, 15.f, FColor::Green, true, -1.f, 0);
+#define DRAW_POINT_SingleFrame(Location) if(GetWorld()) DrawDebugPoint(GetWorld(), Location, 15.f, FColor::Green, false, -1.f, 0);
 #define DRAW_VECTOR(StartLocation, EndLocation) if(GetWorld()) \
     { \
         DrawDebugLine(GetWorld(), StartLocation, EndLocation, FColor::Red, true, -1.f, 0, 1.f); \
