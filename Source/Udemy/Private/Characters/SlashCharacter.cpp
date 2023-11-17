2d0d75ddad9c0b068a69cf178b9100114be647b3
AxlMan31 <axel.woermann@gmail.com>
2023-11-17 08:08:48 +0100

    !U Blocked Movement during EAS_Attacking

--- Source/Udemy/Private/Characters/SlashCharacter.cpp
+++ Source/Udemy/Private/Characters/SlashCharacter.cpp
@@ -76,6 +76,7 @@ void ASlashCharacter::SetupPlayerInputComponent(UInputComponent *PlayerInputComp
 
 void ASlashCharacter::Move(const FInputActionValue &Value)
 {
+	if (ActionState == EActionState::EAS_Attacking) return;
 	const FVector2D MovementVector = Value.Get<FVector2D>();
 
 	// find which way is forward
