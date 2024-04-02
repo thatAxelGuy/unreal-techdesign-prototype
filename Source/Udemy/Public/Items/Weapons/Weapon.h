2a8529b7d771302cbe38e914615ccccc93d48f99
Axel Woermann <53866066+AxlMan31@users.noreply.github.com>
2024-04-02 21:17:35 +0200

    !A Added box component to weapon

--- Source/Udemy/Public/Items/Weapons/Weapon.h
+++ Source/Udemy/Public/Items/Weapons/Weapon.h
@@ -7,6 +7,7 @@
 #include "Weapon.generated.h"
 
 class USoundBase;
+class UBoxComponent;
 
 /**
  * 
@@ -16,6 +17,7 @@ class UDEMY_API AWeapon : public AItem
 {
 	GENERATED_BODY()
 public:
+    AWeapon();
     void Equip(USceneComponent *InParent, FName InSocketName);
 
     void AttachMeshToSocket(USceneComponent *InParent, const FName &InSocketName);
@@ -24,7 +26,10 @@ protected:
     virtual void OnSphereOverlap(UPrimitiveComponent *OverlappedComponent, AActor *OtherActor, UPrimitiveComponent *OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult &SweepResult) override;
     virtual void OnSphereEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex) override;
 private:
-    UPROPERTY(EditAnywhere, Category = "WeaponProperties")
+    UPROPERTY(EditAnywhere, Category = "Weapon Properties")
     USoundBase* EquipSound;
+
+    UPROPERTY(VisibleAnywhere, Category = "Weapon Properties")
+    UBoxComponent* WeaponBox;
 	
 };
