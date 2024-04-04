90b89a951675a70037cba2b2a8ce912bc9cefa30
Axel Woermann <53866066+AxlMan31@users.noreply.github.com>
2024-04-04 11:07:27 +0200

    !U Moved weapon box trace to cpp

--- Source/Udemy/Public/Items/Weapons/Weapon.h
+++ Source/Udemy/Public/Items/Weapons/Weapon.h
@@ -23,13 +23,23 @@ public:
     void AttachMeshToSocket(USceneComponent *InParent, const FName &InSocketName);
 
 protected:
+    virtual void BeginPlay() override;
     virtual void OnSphereOverlap(UPrimitiveComponent *OverlappedComponent, AActor *OtherActor, UPrimitiveComponent *OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult &SweepResult) override;
     virtual void OnSphereEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex) override;
+
+    UFUNCTION()
+    void OnBoxOverlap(UPrimitiveComponent *OverlappedComponent, AActor *OtherActor, UPrimitiveComponent *OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult &SweepResult);
 private:
     UPROPERTY(EditAnywhere, Category = "Weapon Properties")
     USoundBase* EquipSound;
 
     UPROPERTY(VisibleAnywhere, Category = "Weapon Properties")
     UBoxComponent* WeaponBox;
+
+    UPROPERTY(VisibleAnywhere)
+    USceneComponent* BoxTraceStart;
+
+    UPROPERTY(VisibleAnywhere)
+    USceneComponent* BoxTraceEnd;
 	
 };
