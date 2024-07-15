1682ba25039b8a126e93cc1e9eddfe60e0238b0f
Axel Woermann <53866066+AxlMan31@users.noreply.github.com>
2024-07-15 18:48:56 +0200

    !UA Added SFX Meta sounds for potbreaks with BP Native event

--- Source/Udemy/Private/Items/Weapons/Weapon.cpp
+++ Source/Udemy/Private/Items/Weapons/Weapon.cpp
@@ -95,7 +95,7 @@ void AWeapon::OnBoxOverlap(UPrimitiveComponent *OverlappedComponent, AActor *Oth
         IHitInterface* HitInterface = Cast<IHitInterface>(BoxHit.GetActor());
         if(HitInterface)
         {
-            HitInterface->GetHit(BoxHit.ImpactPoint);
+            HitInterface->Execute_GetHit(BoxHit.GetActor(), BoxHit.ImpactPoint);
         }
         ActorsToIgnore.AddUnique(BoxHit.GetActor());
         CreateFields(BoxHit.ImpactPoint);
